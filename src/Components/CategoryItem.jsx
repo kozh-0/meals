3c6a632071d3d9a6c0e6b0b49ea001e617e3ea28
kozh-0 <kdaufa@mail.ru>
2022-02-19 00:53:15 +0500

    2nd

--- src/Components/CategoryItem.jsx
+++ src/Components/CategoryItem.jsx
@@ -10,7 +10,7 @@ export default function CategoryItem(props) {
             </div>
             <div className="card-content">
                 <span className="card-title">{strCategory}</span>
-                <p>{strCategoryDescription.slice(0, 70)}...</p>
+                <p>{strCategoryDescription.slice(0, 85)}...</p>
             </div>
             <div className="card-action">
                 <Link to={`/category/${idCategory}`} className="btn">Watch category</Link>
