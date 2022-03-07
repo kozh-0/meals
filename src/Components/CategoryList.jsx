782f110e1ab0f78246a848356e9cc42f02e4b806
kozh-0 <kdaufa@mail.ru>
2022-03-07 16:29:19 +0300

    filter check

--- src/Components/CategoryList.jsx
+++ src/Components/CategoryList.jsx
@@ -1,12 +1,12 @@
-import CategoryItem from "./CategoryItem"
+import CategoryItem from "./CategoryItem";
 
 export default function CategoryList({ catalog = [] }) {
 
     return (
         <div className="CategoryList">
-            {catalog.map(item => (
+            {catalog.length ? (catalog.map(item => (
                 <CategoryItem key={item.idCategory} {...item}/>
-            ))}
+            ))) : <h3>No result</h3>}
         </div>
     )
 }
